9cea6651d6f1ae3ee7a72a36f1b90b054b2397f4
Ari Gebhardt <ari.gebhardt@cooper.edu>
2024-11-12 00:19:14 -0500

    Added Whisper.embed()

--- whisper/embed.py
+++ whisper/embed.py
@@ -3,7 +3,7 @@ from typing import Union
 import numpy as np
 import torch
 
-from .audio import log_mel_spectrogram, N_SAMPLES
+from .audio import log_mel_spectrogram, N_SAMPLES, N_FRAMES
 
 # @torch.no_grad
 def embed(
@@ -11,13 +11,14 @@ def embed(
     audio: Union[str, np.ndarray, torch.Tensor],
 ):
     mel = log_mel_spectrogram(audio, model.dims.n_mels, padding=N_SAMPLES)
-    
-    if single := mel.ndim == 2:
-        mel = mel.unsqueeze(0)
 
-    trimmed_length = (mel.shape[1] // model.dims.n_audio_ctx) * model.dims.n_audio_ctx
+    trimmed_length = (mel.shape[1] // N_FRAMES) * N_FRAMES
     mel = mel[:, :trimmed_length]
-    mel = mel.reshape(-1, model.dims.n_mels, model.dims.n_audio_ctx)
-    print(mel.shape)
+    mel = mel.reshape(
+        trimmed_length // N_FRAMES,
+        model.dims.n_mels,
+        N_FRAMES,
+    )
 
-    # return model.embed_audio(mel)
+    embedding = model.embed_audio(mel)
+    return embedding.view(-1, 384)
