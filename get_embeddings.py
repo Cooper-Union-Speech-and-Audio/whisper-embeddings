d7152e3aedf793837d4449bb6326c646b2deebe0
Ari Gebhardt <ari.gebhardt@cooper.edu>
2024-12-06 11:38:31 -0500

    .

--- get_embeddings.py
+++ get_embeddings.py
@@ -3,12 +3,13 @@ import os
 import pandas as pd
 import pickle
 import torch
-from tqdm import tqdm
+from tqdm import trange
 
 import whisper
 
 torch.cuda.empty_cache()
 model = whisper.load_model("large-v2")
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 audio_dir = "../PROCESS-V1"
 
@@ -17,21 +18,24 @@ metadata = pd.read_csv(metadata_file)
 
 extractions = []
 
-for folder in tqdm(os.listdir(audio_dir)):
-    if folder.startswith("Process-rec-"):
-        extraction = {}
-        extraction["metadata"] = metadata[metadata["Record-ID"] == folder].iloc[0].to_dict()
-        for test in ["CTD", "PFT", "SFT"]:
-            audio_fp = os.path.join(audio_dir, folder, f"{folder}__{test}.wav")
+for n in range(6, 16):
+    for i in trange(n*10 + 1, min((n+1) * 10 + 1, 158)):
+        folder = f"Process-rec-{i:03d}"
+        if folder.startswith("Process-rec-"):
+            extraction = {}
+            extraction["metadata"] = metadata[metadata["Record-ID"] == folder].iloc[0].to_dict()
+            for test in ["CTD", "PFT", "SFT"]:
+                audio_fp = os.path.join(audio_dir, folder, f"{folder}__{test}.wav")
 
-            audio = whisper.load_audio(audio_fp)
-            with torch.no_grad():
-                embeddings = model.embed(audio)
+                audio = whisper.load_audio(audio_fp)
+                audio = torch.from_numpy(audio).to(device)
+                with torch.no_grad():
+                    embeddings = model.embed(audio)
 
-            print(embeddings.shape)
-            extraction[test] = embeddings
+                print(embeddings.shape)
+                extraction[test] = embeddings
 
-        extractions.append(extraction)
+            extractions.append(extraction)
 
-with open("embeddings.pkl", "wb") as f:
-    pickle.dump(extractions, f)
+    with open(f"embeddings{n}.pkl", "wb") as f:
+        pickle.dump(extractions, f)
--- whisper/embed.py
+++ whisper/embed.py
@@ -3,14 +3,16 @@ from typing import Union
 import numpy as np
 import torch
 
-from .audio import log_mel_spectrogram, N_SAMPLES, N_FRAMES
+from .audio import N_FRAMES, N_SAMPLES, log_mel_spectrogram
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # @torch.no_grad
 def embed(
     model: "Whisper",
     audio: Union[str, np.ndarray, torch.Tensor],
 ):
-    mel = log_mel_spectrogram(audio, model.dims.n_mels, padding=N_SAMPLES)
+    mel = log_mel_spectrogram(audio, model.dims.n_mels, padding=N_SAMPLES).to(device)
 
     trimmed_length = (mel.shape[1] // N_FRAMES) * N_FRAMES
     mel = mel[:, :trimmed_length]
@@ -20,5 +22,5 @@ def embed(
         N_FRAMES,
     )
 
-    embedding = model.embed_audio(mel)
+    embedding = model.embed_audio(mel).to('cpu')
     return embedding.view(-1, 384)
